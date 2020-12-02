9a3b539e039bd2a44ee527bb42f2b8d8679e5628
dhrubabasu <dhruba@avalabs.org>
2020-12-01 19:33:31 -0500

    bump deps, update tailwind import

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,7 @@
-<style>
-@import "tailwindcss/tailwind.css";
+<style global lang="postcss">
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
 </style>
 
 <slot />
